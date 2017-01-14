d709047c8c90c44dde9daf2362674238b2f635c0
Zarkon <leancher@yandex.ru>
2017-01-14 19:20:27 +0300

    Вторая версия платы. Доработана прошивка для текущей платы.

--- AL.Car.SmartPS.Fw/main.c
+++ AL.Car.SmartPS.Fw/main.c
@@ -10,29 +10,21 @@
 
 #define DEV_NAME "PS board 1.0"
 #define ADC_VOLT_MULTIPLIER_MV		(68+2.2)/2.2 * 1.1
-#define DELAY_BEFOR_CONNECT 100 //10 000 мс / 100 циклов
+#define DIODE_CORRECTION 200
 
 #define VOLTAGE_INPUT_ENGINE_RUN 13000
-#define VOLTAGE_INPUT_NORMAL 12500
+#define VOLTAGE_INPUT_NORMAL 12000
 #define VOLTAGE_BATTERY_DISCHARGEON 12500
-#define VOLTAGE_BATTERY_DISCHARGEOFF 11800
+#define VOLTAGE_BATTERY_DISCHARGEOFF 10000
 
-typedef enum
-{
-	BATTERY_DISCHARGED=1,
-	BATTERY_CHARGED=2,
-	BATTERY_CHARGING=3,
-	BATTERY_DISCHARGING=4,
-} DEVICE_STATE;
-DEVICE_STATE current_state = BATTERY_DISCHARGED;
-
-int counter = 0;
+int voltage_input=0;
+int voltage_battery=0;
 
 int get_voltage_input()
 {
 	adc_init_voltage_input();
 	int val=0;
-	val=adc_read_average(3)*ADC_VOLT_MULTIPLIER_MV;
+	val=adc_read_average(3)*ADC_VOLT_MULTIPLIER_MV+DIODE_CORRECTION;
 	return val;
 }
 
@@ -40,7 +32,7 @@ int get_voltage_battery()
 {
 	adc_init_voltage_battery();
 	int val=0;
-	val=adc_read_average(3)*ADC_VOLT_MULTIPLIER_MV;
+	val=adc_read_average(3)*ADC_VOLT_MULTIPLIER_MV+DIODE_CORRECTION;
 	return val;
 }
 
@@ -54,52 +46,77 @@ void device_init()
 int main(void)
 {
 	wdt_enable(WDTO_8S);
-	
-	led_yellow_set(1);
-	
+	device_init();	
     while (1) 
     {
 		wdt_reset();
-
-		if (get_voltage_input()<VOLTAGE_INPUT_NORMAL)
-		{
-			relay_charge_battery(0);
-			led_green_set(0);
-			led_yellow_set(0);
-			led_red_set(0);
-			if (get_voltage_battery()<VOLTAGE_BATTERY_DISCHARGEOFF)
-			{
-				relay_power_supply_set(0);
-			}
-		}
-
-		if (get_voltage_input()>VOLTAGE_INPUT_NORMAL)
-		{
-			if (get_voltage_battery()<VOLTAGE_BATTERY_DISCHARGEOFF)
-			{
-				led_green_set(0);
-				led_yellow_set(0);				
-				led_red_set(1);
-			}
-		}
-
-		if (get_voltage_input()>VOLTAGE_INPUT_ENGINE_RUN)
+		
+		voltage_input=get_voltage_input();
+		voltage_battery=get_voltage_battery();
+		
+		//Двигатель заведен
+		if (voltage_input>VOLTAGE_INPUT_ENGINE_RUN)
 		{
 			relay_power_supply_set(1);
-			if (get_voltage_battery()<VOLTAGE_BATTERY_DISCHARGEON)
+			//Батарея разряжена, включаем заряд
+			if (voltage_battery<VOLTAGE_BATTERY_DISCHARGEON)
 			{
 				relay_charge_battery(1);
 				led_green_set(0);
 				led_yellow_set(1);
 				led_red_set(0);
 			}
+			else
+			{
+				//Батарея заряжена, выключаем заряд
+				relay_charge_battery(0);
+				led_yellow_set(0);
+				led_green_set(1);
+				led_red_set(0);
+			}
 		}
-		if (get_voltage_battery()>VOLTAGE_BATTERY_DISCHARGEON)
+		//Включено зажигание
+		if (voltage_input>VOLTAGE_INPUT_NORMAL)
 		{
+			//Двигатель не заведен
+			if (voltage_input<VOLTAGE_INPUT_ENGINE_RUN)
+			{
+				//Отключаем реле зарядки
+				relay_charge_battery(0);
+				//Включаем реле нагрузки
+				relay_power_supply_set(1);
+				//Проверяем напряжение батареи, батарея разряжена
+				if (voltage_battery<VOLTAGE_BATTERY_DISCHARGEOFF)
+				{
+					led_green_set(0);
+					led_yellow_set(0);				
+					led_red_set(1);
+					//Отключаем нагрузку
+					//relay_power_supply_set(0);
+				}
+				else
+				{
+					//Батарея заряжена, оставляем нагрузку
+					led_green_set(0);
+					led_yellow_set(1);
+					led_red_set(0);				
+				}				
+			}
+
+		}
+		//Зажигание выключено
+		if (voltage_input<VOLTAGE_INPUT_NORMAL)
+		{
+			//Отключаем заряд
 			relay_charge_battery(0);
+			led_green_set(0);
 			led_yellow_set(0);
-			led_green_set(1);
 			led_red_set(0);
+			//Батарея сильно разряжена, все выключаем
+			if (voltage_battery<VOLTAGE_BATTERY_DISCHARGEOFF)
+			{
+			relay_power_supply_set(0);
+			}
 		}
 		_delay_ms(100);
     }
